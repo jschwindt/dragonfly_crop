f2338dc23ae2c1e749a63494d632265b52d2af1b
Juan Schwindt <juan@schwindt.org>
2010-09-04 01:16:59 -0300

    Custom shortcut not necessary

--- config/initializers/dragonfly_images.rb
+++ config/initializers/dragonfly_images.rb
@@ -1,12 +1,7 @@
 require 'dragonfly/rails/images'
 
-app = Dragonfly[:images]
-
-app.configure do |c|
-  
-  c.job :custom_crop do |size|
-    md = size.match('(\d+):(\d+):(\d+)x(\d+)')
-    process :crop, :x => md[1], :y => md[2],  :width  => md[3], :height => md[4]
-  end
-
-end
+# app = Dragonfly[:images]
+# 
+# app.configure do |c|
+# 
+# end
