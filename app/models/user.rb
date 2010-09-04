a960185b8fa953c764c2970d24a932cf65c3c5aa
Juan Schwindt <juan@schwindt.org>
2010-09-03 23:49:51 -0300

    Validates users name

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,11 @@ class User < ActiveRecord::Base
   THUMB_W = 80
   THUMB_H = 60
   CROP_W = 400
-  
+
+  validates_format_of :name, :with => /\A[a-z0-9]+\z/i
+
   image_accessor         :avatar
   validates_presence_of  :avatar
   validates_mime_type_of :avatar, :in => %w(image/jpeg image/png image/gif)
-  
+
 end
