a82ed2dd520929240fd3823df072ff6679eb4c74
Juan Schwindt <juan@schwindt.org>
2010-09-04 00:41:50 -0300

    Custom cropping with shortcut

--- config/initializers/dragonfly_images.rb
+++ config/initializers/dragonfly_images.rb
@@ -1,1 +1,12 @@
 require 'dragonfly/rails/images'
+
+app = Dragonfly[:images]
+
+app.configure do |c|
+  
+  c.job :custom_crop do |size|
+    md = size.match('(\d+):(\d+):(\d+)x(\d+)')
+    process :crop, :x => md[1], :y => md[2],  :width  => md[3], :height => md[4]
+  end
+
+end
